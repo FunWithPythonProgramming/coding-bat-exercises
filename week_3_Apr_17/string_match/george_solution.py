94ebff1b7e135f293f020d07459a2de7e2e9d7b3
George Kim <gjkim44@gmail.com>
2020-06-04 07:50:14 -0700

    adding example assignment

--- week_3_Apr_17/string_match/george_solution.py
+++ week_3_Apr_17/string_match/george_solution.py
@@ -11,5 +11,15 @@ string_match('abc', 'axc') → 0
 
 """
 
-def string_match():
-    
+def string_match(a,b):
+    count = 0
+    for x in range(len(a) - 1):
+        if a[x:x+2] == b[x:x+2]:
+            count += 1
+    return count 
+
+
+
+print(string_match('xxcaazz', 'xxbaaz'))
+print(string_match('abc', 'abc')) 
+print(string_match('abc', 'axc')) 
