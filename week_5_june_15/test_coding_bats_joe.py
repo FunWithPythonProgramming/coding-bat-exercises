dcf58c2c54aa333aa18ff64893b4ae843e968f8e
Gentsch <gentsch@amazon.com>
2020-06-17 20:04:18 -0400

    adding my tests from coding bat caught_speeding

---
+++ week_5_june_15/test_coding_bats_joe.py
@@ -0,0 +1,15 @@
+import caught_speeding_joe
+
+def test_caught_speeding():
+    assert caught_speeding_joe.test_caught_speeding(60, False) == 0
+    assert caught_speeding_joe.test_caught_speeding(65, False) ==1
+    assert caught_speeding_joe.test_caught_speeding(65, True) == 0
+    assert caught_speeding_joe.test_caught_speeding(80, False) == 1
+    assert caught_speeding_joe.test_caught_speeding(85, False) == 2
+    assert caught_speeding_joe.test_caught_speeding(85, True) == 1
+    assert caught_speeding_joe.test_caught_speeding(70, False) == 1
+    assert caught_speeding_joe.test_caught_speeding(75, False) == 1
+    assert caught_speeding_joe.test_caught_speeding(75, True) == 1
+    assert caught_speeding_joe.test_caught_speeding(40, False) == 0
+    assert caught_speeding_joe.test_caught_speeding(40, True) == 0
+    assert caught_speeding_joe.test_caught_speeding(90, False) == 2
